c52d7997848dcbcf2719155c63ec3cbcd1974f1e
Marteen Motun Mubaraq <motunmarteen@gmail.com>
2024-05-13 23:27:29 +0100

    declaring variables of each basic data types

---
+++ declaration.js
@@ -0,0 +1,7 @@
+// Basic Data Types: Write JavaScript code to declare variables of each basic data type (string, number, boolean, null, undefined).
+
+let strings = "I am a man"
+let age = 23
+let isMan = true
+let student = null
+let numbers;
