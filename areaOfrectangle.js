4fcf78a46f592963e6026d8f0651197ee4a3ba13
Marteen Motun Mubaraq <motunmarteen@gmail.com>
2024-05-13 23:33:58 +0100

    Calculating the area of a rectangle

---
+++ areaOfrectangle.js
@@ -0,0 +1,8 @@
+// Arithmetic operators: Write JavaScript code to calculate the area of a rectangle given its length and width.
+
+let lengths = Number(prompt("what is the length of the rectangle? : "))
+let widths = Number(prompt("what is the width of the rectangle? : "))
+
+let areaOfRectangle = lengths * widths
+
+console.log(areaOfRectangle)
