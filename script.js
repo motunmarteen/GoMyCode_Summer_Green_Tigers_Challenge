223879c172d1c425b2c26dcd85cf0e7a88827aa6
Marteen Motun Mubaraq <motunmarteen@gmail.com>
2024-05-13 22:51:59 +0100

    A program that uses alert to print welcome to my world on the web page

---
+++ script.js
@@ -0,0 +1,3 @@
+// Adding JavaScript to a Web Page: Create an HTML file and include a <script> tag to load a JavaScript file named "script.js". In "script.js", write code to alert the user with "Welcome to My Website!".
+
+alert("Welcome to My Website!");
