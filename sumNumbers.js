1c09564d295e37aa1ccf961354cdf7fe26088dcf
Marteen Motun Mubaraq <motunmarteen@gmail.com>
2024-05-13 23:18:57 +0100

    A program that calculates the sum of 2 numbers

---
+++ sumNumbers.js
@@ -0,0 +1,6 @@
+// console.log: Write JavaScript code that calculates the sum of two numbers (e.g., 5 and 10) and logs the result to the console.
+
+let numA = Number(prompt('Enter your first Number here for calculation'))
+let numB = Number(prompt('Enter your second Number here for calculation'))
+
+console.log(`The sum of the 2 numbers ${numA} and ${numB} is ${numA + numB}`);
