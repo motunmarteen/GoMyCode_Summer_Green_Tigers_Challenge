1361f189e5cbff59096618c39d9ffeca48fbdf7c
Marteen Motun Mubaraq <motunmarteen@gmail.com>
2024-05-13 22:41:16 +0100

    GoMyCode Summer Green Tigers Challenge

---
+++ questions.js
@@ -0,0 +1,39 @@
+// Creating Your First JavaScript: Write a JavaScript program that prints "Hello, World!" to the console.
+
+// Adding JavaScript to a Web Page: Create an HTML file and include a <script> tag to load a JavaScript file named "script.js". In "script.js", write code to alert the user with "Welcome to My Website!".
+
+// Alert, Prompt & Confirm: Write a JavaScript program that prompts the user to enter their age. If the age is greater than or equal to 18, display an alert saying "You are an adult". Otherwise, display an alert saying "You are a minor".
+
+// console.log: Write JavaScript code that calculates the sum of two numbers (e.g., 5 and 10) and logs the result to the console.
+
+// Basic Data Types: Write JavaScript code to declare variables of each basic data type (string, number, boolean, null, undefined).
+
+// Arithmetic operators: Write JavaScript code to calculate the area of a rectangle given its length and width.
+
+// Assignment Operators: Write JavaScript code to increment a variable by 5 using both the = and += operators.
+
+// Comparison Operators: Write JavaScript code to compare two numbers and log whether they are equal or not to the console.
+
+// Logical or Boolean Operators: Write JavaScript code to check if a number is both greater than 10 and less than 20.
+
+// String Operators: Write JavaScript code to concatenate two strings together.
+
+// Program Flow: Functions: Write a JavaScript function that takes two parameters and returns their sum.
+
+// User-Defined Functions: Write a JavaScript function to calculate the factorial of a given number.
+
+// Function Parameters: Write a JavaScript function that takes a radius as a parameter and calculates the area of a circle.
+
+// Calling Functions & The Return Statement: Write JavaScript code to call the function you created in the question above and log the result to the console.
+
+// Program Flow: Conditional Statement: Write JavaScript code to check if a number is positive, negative, or zero.
+
+// The if Statement: Write JavaScript code that uses an if statement to check if a user's age is greater than or equal to 18.
+
+// The else Statement: Write JavaScript code that uses an else statement to display a message if a user's age is less than 18.
+
+// The else if Statement: Write JavaScript code that uses else if statements to categorize a user's age into different age groups.
+
+// The switch Statement: Write JavaScript code that uses a switch statement to determine the day of the week based on a given number.
+
+// Program Flow: Loops: Write JavaScript code that uses a for loop to display numbers from 1 to 10 on the console.
