1f84b8da83a3d1fc56e1eac2355f25ee2770d9f8
Marteen Motun Mubaraq <motunmarteen@gmail.com>
2024-05-14 07:06:16 +0100

    COmparing 2 number to show if they are the same or not

---
+++ comparison.js
@@ -0,0 +1,10 @@
+// Comparison Operators: Write JavaScript code to compare two numbers and log whether they are equal or not to the console.
+
+let num1 = Number(prompt("type in the first number for comparison"))
+let num2 = Number(prompt("Type in the second number for comparison"))
+
+if (num1 === num2) {
+    console.log(`${num1} and ${num2} are the same`)
+} else {
+    console.log(`${num1} and ${num2} are not the same`)
+}
