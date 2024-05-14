40ab8daacc4b04f0600535b843961f0935c3d3e6
Marteen Motun Mubaraq <motunmarteen@gmail.com>
2024-05-14 07:21:17 +0100

    A program that checks if a anumber is gretaer than or less thand a dna aprogram that concatenates strings

---
+++ boolean.js
@@ -0,0 +1,9 @@
+// Logical or Boolean Operators: Write JavaScript code to check if a number is both greater than 10 and less than 20.
+
+let num = Number(prompt("Enter a number to check : "))
+
+if (num > 10 && num < 20) {
+    console.log(`${num} is greater than 10 and less than 20`)
+} else {
+    console.log(`${num} is just there and nothing much`)
+}
---
+++ concatenate.js
@@ -0,0 +1,8 @@
+// String Operators: Write JavaScript code to concatenate two strings together.
+
+let string1 = prompt("Type in your statement here ")
+let string2 = prompt("Type in your statement here ")
+let string3 = (" ")
+
+let strings = string1 + string3 + string2
+console.log(strings)
